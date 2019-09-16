4f458d03fade66398de480582e14c91d13916d02
Unknown <stafford.carh@gmail.com>
2019-09-16 15:37:21 -0400

    app_tests
    added tests for the app itself, have a couple of starting skeletons for tests

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -5,15 +5,31 @@ app.config['TESTING'] = True
 web = app.test_client()
 
 
-def test_index():
+def test_app():
     rv = web.get('/', follow_redirects=True)
-    assert_equal(rv.status_code, 404)
-
-    rv = web.get('/hello', follow_redirects=True)
     assert_equal(rv.status_code, 200)
-    assert_in(b"Fill Out This Form", rv.data)
+    assert_in(b"waking up on the catwalk", rv.data)
 
-    data = {'name': 'Zed', 'greet': 'Hola'}
-    rv = web.post('/hello', follow_redirects=True, data=data)
-    assert_in(b"Zed", rv.data)
-    assert_in(b"Hola", rv.data)
+
+def test_session():
+    with web.session_transaction() as sess:
+        sess['room_name'] = 'laser_weapon_armory'
+    rv = web.get('/game', follow_redirects=True)
+    assert_in(b"locked with a 3 digit", rv.data)
+
+
+def test_session_next():
+    with web.session_transaction() as sess:
+        sess['room_name'] = 'laser_weapon_armory'
+    rv = web.post('/game', data='action=012',
+                  content_type='application/x-www-form-urlencoded', follow_redirects=True)
+    assert_in(b"maintain a slow heart", rv.data)
+
+    # rv = web.get('/hello', follow_redirects=True)
+    # assert_equal(rv.status_code, 200)
+    # assert_in(b"Fill Out This Form", rv.data)
+
+    # data = {'name': 'Zed', 'greet': 'Hola'}
+    # rv = web.post('/hello', follow_redirects=True, data=data)
+    # assert_in(b"Zed", rv.data)
+    # assert_in(b"Hola", rv.data)
